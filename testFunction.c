d0bbc1238965b64d8c145031baa88fa131182b12
carbon <cheeseburgerbbgun@gmail.com>
2022-09-27 18:18:04 -0600

    Added color code to test function

--- testFunction.c
+++ testFunction.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 int test(int success, const char* String) {
 
   if(success) {
+    printf("\033[0;32m");
     printf("\n Success! :  %s\n", String);
+    printf("\033[0m");
   } else {
+    printf("\033[0;31m");
     printf("\n Fail!    :  %s\n", String);
+    printf("\033[0m");
   }
 
   return success;
